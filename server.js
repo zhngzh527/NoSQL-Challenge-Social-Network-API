4b2b292dc4d324bf6d031d2c8aef9632b50d9183
ZHONGCHENG ZHAO <zhongchengzhao@ZHONGCHENGs-MBP.lan>
2023-01-31 06:57:15 -0600

    serverjs

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+mongoose.set("debug", true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
