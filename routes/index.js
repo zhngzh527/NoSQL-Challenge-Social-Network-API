53921f0b4c548e3bf7f5dcad5c59f475ff5ff365
ZHONGCHENG ZHAO <zhongchengzhao@ZHONGCHENGs-MBP.lan>
2023-01-31 19:52:15 -0600

    fix routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-    res.status(404).send('<h1> 404 Error!</h1>');
+    res.status(404).send('404 Error!');
   });
   
   module.exports = router;
